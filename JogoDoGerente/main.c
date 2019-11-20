05827e8f7807d0af54fd936836b747679a85cbbf
Italo luigi Cerqueira Dovera <italo.luigi@ufma.deinf.local>
2019-11-20 17:19:55 -0300

    continua com o arquivo bugado mas ta comentandinho

--- JogoDoGerente/main.c
+++ JogoDoGerente/main.c
@@ -9,33 +9,37 @@ int arrlen(char *arr){
 int main(void) {
   FILE *pf = fopen("cenario/atribuicao_10.txt","r");
   int tam = 301;
-  char string[tam];
+  char string[tam]; //string de tamanho arbitrário para alocação de cada linha do arquivo
   /*for(char c=getc(pf);c!=EOF;pf++){
     printf("%c", c);
   }
   while(fgets(string, 1000, pf) != NULL)
     printf("%s", string);
   */
-  fgets(string, arrlen(string), pf);
-  int tam_matriz = atoi(string);
+  fgets(string, arrlen(string), pf); //recebendo o tamanho da matriz
+  int tam_matriz = atoi(string); //transformando string pra inteiro
   int cenario[tam_matriz][tam_matriz];
 
   for (int i=0; fgets(string, arrlen(string), pf) != NULL && i<tam_matriz; i++){
     int k=0;
     for (int j=0; j<tam_matriz; j++){
-      char strnum[4];
-      for(int l=0; ; k++, l++){
-        if (string[k]==' '){
-          k++;
-          break;
-        }
+      char strnum[4] = "";
+      /*if(string[k]==' '){
+        k++;
+      }*/
+      for(int l=0; string[k]!=' ';){
         strnum[l] = string[k];
+        k++;
+        l++;
       }
       cenario[i][j] = atoi(strnum);
       printf("%d ", cenario[i][j]);
     }
+     printf("aaa\n");
     printf("\n");
   }
+
+
 
   /*for (int i=0; i<tam_matriz; i++){
     for (int j=0; j<tam_matriz; j++){
