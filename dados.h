6dcfb7d71a4b43be570f51c4719956b4a0215768
Daniel Moreira Pinto <48635337+DanielMoreiraPinto@users.noreply.github.com>
2019-12-01 16:50:15 -0300

    Função de salvamento e recuperação de dados incluída

---
+++ dados.h
@@ -0,0 +1,133 @@
+#define MAX 99999
+#define cenario10 "cenario/atribuicao_10.txt"
+#define cenario20 "cenario/atribuicao_20.txt"
+#define cenario30 "cenario/atribuicao_30.txt"
+#define cenario50 "cenario/atribuicao_50.txt"
+#define cenario100 "cenario/atribuicao_100.txt"
+
+typedef struct{
+	int Score10;
+	int Score20;
+	int Score30;
+	int Score50;
+	int Score100;
+}Score;
+
+Score * getRecorde(){
+    FILE *pLeitura = fopen("score.txt", "r");
+		Score recordes;
+		Score *p = &recordes;
+    if (pLeitura != NULL){
+			fscanf(pLeitura, "%d", &p->Score10);
+			fscanf(pLeitura, "%d", &p->Score20);
+			fscanf(pLeitura, "%d", &p->Score30);
+			fscanf(pLeitura, "%d", &p->Score50);
+			fscanf(pLeitura, "%d", &p->Score100);
+			fclose(pLeitura);
+			 return p;
+		}else{
+			printf("\nERRO\n");
+			return NULL;
+		}
+}
+
+void setRecorde(Score *p){
+	FILE *pEscrita = fopen("score.txt","w");
+	if (pEscrita != NULL){
+		fprintf(pEscrita, "%d\n", p->Score10);
+		fprintf(pEscrita, "%d\n", p->Score20);
+		fprintf(pEscrita, "%d\n", p->Score30);
+		fprintf(pEscrita, "%d\n", p->Score50);
+		fprintf(pEscrita, "%d\n", p->Score100);
+		fclose(pEscrita);
+	}else{
+		printf("\nERRO\n");
+	}
+}
+
+int ** getCenario(int cenario){
+	int cen;
+
+	int **Matriz = (int**)malloc(cenario*sizeof(int)*2);
+	for (int i  = 0; i < cenario; i++ )
+		*(Matriz + i) = (int*)malloc(cenario*sizeof(int));
+
+			if (cenario == 10){
+				 	FILE * pLeitura = fopen(cenario10, "r");
+					if (pLeitura != NULL){
+						fscanf(pLeitura, "%d", &cen);
+						for (int i=0;i<10;i++){
+							for (int j=0;j<10;j++){
+								fscanf(pLeitura, "%d", &Matriz[i][j]);
+							//	printf("%d ", Matriz[i][j]);
+						}// for i
+					//	printf("\n");
+							}// for j
+						fclose(pLeitura);
+					}else
+						printf("\nERRO NO PONTEIRO DE ARQUIVO DO CENARIO 10\n");
+			}else if (cenario == 20){
+					FILE * pLeitura = fopen(cenario20, "r");
+					if (pLeitura != NULL){
+					fscanf(pLeitura, "%d", &cen);
+					for (int i=0;i<20;i++){
+						for (int j=0;j<20;j++){
+							fscanf(pLeitura, "%d", &Matriz[i][j]);
+					//		printf("%d ", Matriz[i][j]);
+					}// for i
+				//	printf("\n");
+						}// for j
+					fclose(pLeitura);
+					}else{
+							printf("\nERRO NO PONTEIRO DE ARQUIVO DO CENARIO 20\n");
+					}
+			}else if (cenario == 30){
+					FILE * pLeitura = fopen(cenario30, "r");
+					if (pLeitura != NULL){
+					fscanf(pLeitura, "%d", &cen);
+					for (int i=0;i<30;i++){
+						for (int j=0;j<30;j++){
+							fscanf(pLeitura, "%d", &Matriz[i][j]);
+					//		printf("%d ", Matriz[i][j]);
+					}// for i
+				//	printf("\n");
+						}// for j
+					fclose(pLeitura);
+					}else{
+							printf("\nERRO NO PONTEIRO DE ARQUIVO DO CENARIO 30\n");
+					}
+			}else if (cenario == 50){
+					FILE * pLeitura = fopen(cenario50, "r");
+					if (pLeitura != NULL){
+					fscanf(pLeitura, "%d", &cen);
+					for (int i=0;i<50;i++){
+						for (int j=0;j<50;j++){
+							fscanf(pLeitura, "%d", &Matriz[i][j]);
+						//	printf("%d ", Matriz[i][j]);
+					}// for i
+			//		printf("\n");
+						}// for j
+					fclose(pLeitura);
+					}else{
+							printf("\nERRO NO PONTEIRO DE ARQUIVO DO CENARIO 50\n");
+					}
+			}else if(cenario == 100){
+				FILE * pLeitura = fopen(cenario100, "r");
+					if (pLeitura != NULL){
+					fscanf(pLeitura, "%d", &cen);
+					for (int i=0;i<100;i++){
+						for (int j=0;j<100;j++){
+							fscanf(pLeitura, "%d", &Matriz[i][j]);
+						//	printf("%d ", Matriz[i][j]);
+					}// for i
+				//	printf("\n");
+						}// for j
+					fclose(pLeitura);
+					}else{
+							printf("\nERRO NO PONTEIRO DE ARQUIVO DO CENARIO 100\n");
+					}
+			}else{
+				printf("\nVALOR INFORMADO INVALIDO\n");
+			}
+			return Matriz;
+}
