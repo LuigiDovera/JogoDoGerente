fc454833e6a68da375a39751206c434aefa52b04
Daniel Moreira Pinto <48635337+DanielMoreiraPinto@users.noreply.github.com>
2019-12-11 14:18:37 -0300

    TA FUNCIONANDO(falta o timer)

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 
 int main() {
 
-    setlocale(LC_ALL, "Portuguese");//adicionando acentuação
+    setlocale(LC_ALL, "Portuguese");//adicionando acentuaï¿½ï¿½o
 
     int opcaoMenu, opcaoIA, opcaoCenario;
 
@@ -21,7 +21,7 @@ int main() {
 
     while(TRUE){
         system("cls");
-        printf("Bem vindo ao Jogo da Atribuição!\n");
+        printf("Bem vindo ao Jogo da Atribuiï¿½ï¿½o!\n");
         printf("    1 - JOGAR SOZINHO\n    2 - VERSUS\n    3 - I.A.\n    4 - RECORDE\n    5 - SAIR\n\n");
         scanf("%d", &opcaoMenu);
 
@@ -60,17 +60,17 @@ int main() {
                         int BOOL;
                         do{
                             BOOL = FALSE;
-                            printf("Atribua uma Tarefa ao Funcionário %d: ", i+1);
+                            printf("Atribua uma Tarefa ao Funcionï¿½rio %d: ", i+1);
                             scanf("%d", &(jo->atribuicoes[i]));
                             jo->atribuicoes[i]--;
                             if(jo->atribuicoes[i]<0 || jo->atribuicoes[i]>=opcaoCenario){
-                                printf("Essa tarefa não existe. Escolha novamente.\n");
+                                printf("Essa tarefa nï¿½o existe. Escolha novamente.\n");
                                 BOOL = TRUE;
                                 continue;
                             }
                             for(int j=0; j<i; j++){
                                 if(jo->atribuicoes[j]==jo->atribuicoes[i]){
-                                    printf("Você já atribuiu essa Tarefa a outro Funcionário. Escolha novamente.\n");
+                                    printf("Vocï¿½ jï¿½ atribuiu essa Tarefa a outro Funcionï¿½rio. Escolha novamente.\n");
                                     BOOL = TRUE;
                                     break;
                                 }
@@ -83,7 +83,7 @@ int main() {
                         jo->resultado += mm[i][jo->atribuicoes[i]];
                     }
 
-                    printf("Sua pontuação: %d\n", jo->resultado);
+                    printf("Sua pontuaï¿½ï¿½o: %d\n", jo->resultado);
 
                     Score p = getRecorde();
 
@@ -108,19 +108,137 @@ int main() {
 
                     break;
                 }else{
-                    printf("Opção inválida. Escolha novamente.\n");
+                    printf("Opï¿½ï¿½o invï¿½lida. Escolha novamente.\n");
                     system("pause");
                     continue;
                 }
 
             }
         }else if(opcaoMenu==2){
+            int BOOL, rIa, rPlayer, **mm;
+            Jogada joIa;
+            do{
+                BOOL = TRUE;
+                printf("Escolha o nï¿½vel de precisï¿½o da IA: 1-Alto, 2-Mï¿½dio, 3-Baixo\n");
+                scanf("%d", &opcaoIA);
+                if(opcaoIA==1 || opcaoIA==2 || opcaoIA==3){
+                    while(TRUE){
+                        printf("Escolha uma Fase: 10, 20, 30 ou 50.\n");
+                        scanf("%d", &opcaoCenario);
+                        printf("\n");
+                        if(opcaoCenario==10 || opcaoCenario==20 || opcaoCenario==30 || opcaoCenario==50 || opcaoCenario==100){
+                            mm = getCenario(opcaoCenario);
 
+                            joIa = jogadaAutomatica(mm, opcaoCenario, 1);
+                            rIa = joIa.resultado;
+                           
+                            BOOL = FALSE;
+
+                            freeIntMatriz(mm, opcaoCenario);
+                            free(joIa.atribuicoes);
+
+                            break;
+                        }else{
+                            printf("Opï¿½ï¿½o invï¿½lida. Escolha novamente.\n");
+                            system("pause");
+                            continue;
+                        }
+                    }
+                }else{
+                    printf("Opï¿½ï¿½o invï¿½lida. Escolha novamente.\n");
+                    system("pause");
+                    continue;
+                }
+            }while(BOOL);
+
+            while(TRUE){
+                //printf("Escolha uma Fase: 10, 20, 30 ou 50.\n");
+                //scanf("%d", &opcaoCenario);
+                if(opcaoCenario==10 || opcaoCenario==20 || opcaoCenario==30 || opcaoCenario==50 || opcaoCenario==100){
+                    system("cls");
+                    mm = getCenario(opcaoCenario);
+                    printf("F\\T ");
+                    for(int i=1;i<=opcaoCenario;i++)
+                        printf("%03d ", i);
+                    printf("\n");
+
+                    //printando a matriz do cenario
+                    for(int i=0; i<opcaoCenario; i++){
+                        printf("%03d ", i+1);
+
+                        for(int j=0; j<opcaoCenario; j++){
+                            printf("%03d ", mm[i][j]);
+                        }
+                        printf("\n");
+
+                    }
+                    printf("\n");
+
+                    Jogada *jo = (Jogada*)malloc(sizeof(Jogada));
+                    jo->atribuicoes = (int*)malloc(sizeof(int)*opcaoCenario);
+
+                    jo->resultado = 0;
+                    for(int i=0; i<opcaoCenario; i++)
+                        jo->atribuicoes[i] = -1;
+
+                    for(int i=0; i<opcaoCenario; i++){
+                        int BOOL;
+                        do{
+                            BOOL = FALSE;
+                            printf("Atribua uma Tarefa ao Funcionï¿½rio %d: ", i+1);
+                            scanf("%d", &(jo->atribuicoes[i]));
+                            jo->atribuicoes[i]--;
+                            if(jo->atribuicoes[i]<0 || jo->atribuicoes[i]>=opcaoCenario){
+                                printf("Essa tarefa nï¿½o existe. Escolha novamente.\n");
+                                BOOL = TRUE;
+                                continue;
+                            }
+                            for(int j=0; j<i; j++){
+                                if(jo->atribuicoes[j]==jo->atribuicoes[i]){
+                                    printf("Vocï¿½ jï¿½ atribuiu essa Tarefa a outro Funcionï¿½rio. Escolha novamente.\n");
+                                    BOOL = TRUE;
+                                    break;
+                                }
+                            }
+                        }while(BOOL);
+                    }
+                    printf("\n");
+
+                    for(int i=0; i<opcaoCenario; i++){
+                        jo->resultado += mm[i][jo->atribuicoes[i]];
+                    }
+                    rPlayer = jo->resultado;
+
+                    printf("Sua pontuaï¿½ï¿½o: %d\n", rPlayer);
+                    printf("PontuaÃ§Ã£o da IA: %d\n", rIa);
+
+                    if(rIa < rPlayer)
+                        printf("VocÃª perdeu\n");
+                    else if(rIa > rPlayer)
+                        printf("VocÃª venceu\n");
+                    else
+                        printf("Empate\n");
+                    
+                    system("pause");
+
+                    freeIntMatriz(mm, opcaoCenario);
+                    free(jo->atribuicoes);
+                    free(jo);
+                    free(joIa.atribuicoes);
+
+                    break;
+                }else{
+                    printf("Opï¿½ï¿½o invï¿½lida. Escolha novamente.\n");
+                    system("pause");
+                    continue;
+                }
+
+            }
         }else if(opcaoMenu==3){
             int BOOL;
             do{
                 BOOL = TRUE;
-                printf("Escolha o nível de precisão da IA: 1-Alto, 2-Médio, 3-Baixo\n");
+                printf("Escolha o nï¿½vel de precisï¿½o da IA: 1-Alto, 2-Mï¿½dio, 3-Baixo\n");
                 scanf("%d", &opcaoIA);
                 if(opcaoIA==1 || opcaoIA==2 || opcaoIA==3){
                     while(TRUE){
@@ -131,7 +249,7 @@ int main() {
                             int ** mm = getCenario(opcaoCenario);
 
                             Jogada jo = jogadaAutomatica(mm, opcaoCenario, 1);
-                            printf("Pontuação da IA: %d\n", jo.resultado);
+                            printf("Pontuaï¿½ï¿½o da IA: %d\n", jo.resultado);
                             for(int i=0; i < opcaoCenario; i++)
                                 printf("F%d-T%d ", i+1,jo.atribuicoes[i]);
                             printf("\n");
@@ -143,13 +261,13 @@ int main() {
 
                             break;
                         }else{
-                            printf("Opção inválida. Escolha novamente.\n");
+                            printf("Opï¿½ï¿½o invï¿½lida. Escolha novamente.\n");
                             system("pause");
                             continue;
                         }
                     }
                 }else{
-                    printf("Opção inválida. Escolha novamente.\n");
+                    printf("Opï¿½ï¿½o invï¿½lida. Escolha novamente.\n");
                     system("pause");
                     continue;
                 }
@@ -157,17 +275,17 @@ int main() {
 
         }else if(opcaoMenu==4){
             Score p = getRecorde();
-            printf("Melhor pontuação no cenário 10: %d\n", p.Score10);
-            printf("Melhor pontuação no cenário 20: %d\n", p.Score20);
-            printf("Melhor pontuação no cenário 30: %d\n", p.Score30);
-            printf("Melhor pontuação no cenário 50: %d\n", p.Score50);
-            printf("Melhor pontuação no cenário 100: %d\n", p.Score100);
+            printf("Melhor pontuaï¿½ï¿½o no cenï¿½rio 10: %d\n", p.Score10);
+            printf("Melhor pontuaï¿½ï¿½o no cenï¿½rio 20: %d\n", p.Score20);
+            printf("Melhor pontuaï¿½ï¿½o no cenï¿½rio 30: %d\n", p.Score30);
+            printf("Melhor pontuaï¿½ï¿½o no cenï¿½rio 50: %d\n", p.Score50);
+            printf("Melhor pontuaï¿½ï¿½o no cenï¿½rio 100: %d\n", p.Score100);
             system("pause");
 
         }else if(opcaoMenu==5){
             break;
         }else{
-            printf("Opção inválida. Escolha novamente.\n");
+            printf("Opï¿½ï¿½o invï¿½lida. Escolha novamente.\n");
             system("pause");
             continue;
         }
